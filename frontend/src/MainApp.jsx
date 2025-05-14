c6bc8ec6e77c449fe8c3ec8f3b8606f2ea7cb0a9
zimmal-adnan <naziazimmal@gmail.com>
2025-05-14 04:27:40 +0400

    Changed Endpoints

--- frontend/src/MainApp.jsx
+++ frontend/src/MainApp.jsx
@@ -27,7 +27,7 @@ function MainApp({username}){
     }
 
     useEffect(() => {
-        const ws = new WebSocket(`wss://your-backend.onrender.com/ws/${username}`);
+        const ws = new WebSocket(`wss://messaging-application.onrender.com/ws/${username}`);
     
         ws.onopen = () => {
           console.log("WebSocket connected"); 
@@ -75,7 +75,7 @@ function MainApp({username}){
             if (!targetUser) return;
             
             try {
-                const response = await fetch(`https://your-backend.onrender.com/get_messages?user1=${username}&user2=${targetUser}`);
+                const response = await fetch(`https://messaging-application.onrender.com/get_messages?user1=${username}&user2=${targetUser}`);
                 if (response.ok) {
                     const data = await response.json();
                     setMessages(data);
