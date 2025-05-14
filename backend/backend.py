bf9959c757b325cfe83972da2069cb8ae3659d9a
zimmal-adnan <naziazimmal@gmail.com>
2025-05-14 04:20:55 +0400

    Changed Address

--- backend/backend.py
+++ backend/backend.py
@@ -548,4 +548,4 @@ async def websocket_chat(websocket: WebSocket, username: str):
 #---Run server---
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
